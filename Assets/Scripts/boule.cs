58ebf1c714519cbc45eebb2a57e2299d21073121
Joldd <julien.coutouly@gmail.com>
2022-12-20 11:31:57 +0100

    Lancer de boules

--- Assets/Scripts/boule.cs
+++ Assets/Scripts/boule.cs
@@ -1,26 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class boule : MonoBehaviour
 {
     public float angle;
-
-    private Vector3 newPos;
-
     [SerializeField]
     private float shootSpeed;
+    [SerializeField]
+    private Rigidbody2D _rb;
+    public player launcher;
 
     void Start()
     {
-        newPos = transform.position;
+        
+        //Position départ
+        Vector2 newPos = new Vector2(launcher._rb.position.x + 0.6f * launcher._cc.size.x * Mathf.Cos(angle), launcher._rb.position.y + 0.6f * launcher._cc.size.y * Mathf.Sin(angle));
+        _rb.MovePosition(newPos);
+        
+        //Shoot
+        Vector2 force = new Vector2(shootSpeed * Mathf.Cos(angle), shootSpeed * Mathf.Sin(angle));
+        _rb.AddForce(force , ForceMode2D.Impulse);
+        Debug.Log(force);
     }
 
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        newPos.x += shootSpeed * Time.deltaTime * Mathf.Cos(angle);
-        newPos.y += shootSpeed * Time.deltaTime * Mathf.Sin(angle);
-
-        transform.position = newPos;
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,18 +3,20 @@ using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
+    public Rigidbody2D _rb;
+    public CapsuleCollider2D _cc;
     //Move
     public InputActionAsset controls;
     [SerializeField]
     private float speed;
-    private Vector3 newPos;
+    private Vector2 newPos;
 
     //Shoot
     public float angle;
     [SerializeField]
-    private GameObject boule;
-    private Vector3 aimDirection;
-    private Vector3 mousePosition;
+    private boule b;
+    private Vector2 aimDirection;
+    private Vector2 mousePosition;
     private new Camera camera;
 
     //Animation
@@ -23,9 +25,6 @@ public class player : MonoBehaviour
 
     private void Awake()
     {
-        //Move
-        newPos = transform.position;
-
         //Shoot
         camera = Camera.main;
         controls.FindActionMap("Player").FindAction("Shoot").performed += ctx =>
@@ -39,31 +38,36 @@ public class player : MonoBehaviour
     {
         //Shoot
         mousePosition = GetMousePosition();
-        aimDirection = (mousePosition - transform.position).normalized;
-        angle = Mathf.Atan2(aimDirection.y, aimDirection.x);
 
         //Move
         Vector2 inputVector = controls.FindActionMap("Player").FindAction("Movement").ReadValue<Vector2>();
-        newPos.x += inputVector.x * speed * Time.deltaTime;
-        newPos.y += inputVector.y * speed * Time.deltaTime;
-        transform.position = newPos;
+        newPos.x = inputVector.x * speed * Time.deltaTime;
+        newPos.y = inputVector.y * speed * Time.deltaTime;
 
         //Animation
         animator.SetOrientation(inputVector.x, inputVector.y);
     }
+
+    public void FixedUpdate()
+    {
+        //Move
+        _rb.MovePosition(_rb.position + newPos);
+
+        //Shoot
+        aimDirection = (mousePosition - _rb.position).normalized;
+        angle = Mathf.Atan2(aimDirection.y, aimDirection.x);
+    }
+
     public void Shoot()
     {
-        GameObject newBoul = Instantiate<GameObject>(boule);
-        newBoul.GetComponent<boule>().angle = angle;
-        Vector3 newBoulpos = Vector3.zero;
-        newBoulpos.x = transform.position.x;
-        newBoulpos.y = transform.position.y;
-        newBoulpos.z = transform.position.z;
-        newBoul.transform.position = newBoulpos;
+        boule newBoul = Instantiate(b);
+        newBoul.angle = angle;
+        newBoul.launcher = this;
+       
         animator.ShootToward(GetMousePosition().x, GetMousePosition().y);
     }
 
-    public Vector3 GetMousePosition()
+    public Vector2 GetMousePosition()
     {
         if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo, 4000, 0b1 << 6))
         {
@@ -71,7 +75,7 @@ public class player : MonoBehaviour
         }
         else
         {
-            return Vector3.zero;
+            return Vector2.zero;
         }
     }
 }
