e57baae1f90b533bcef054ab826175a39901601e
ErwanE <erwan-eps@orange.fr>
2022-12-22 17:15:14 +0100

    Fix warning when snowball spawn

--- Assets/Scripts/boule.cs
+++ Assets/Scripts/boule.cs
@@ -9,7 +9,7 @@ public class boule : NetworkBehaviour
     private float shootSpeed;
     [SerializeField]
     private Rigidbody2D _rb;
-    public NetworkVariable<ulong> launcherId2 = new NetworkVariable<ulong>(25, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    public NetworkVariable<ulong> launcherId2 = new NetworkVariable<ulong>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
     public ulong launcherId => launcherId2.Value;
     public player launcher => PlayerManager.GetPlayer(launcherId);
     [SerializeField]
@@ -20,7 +20,15 @@ public class boule : NetworkBehaviour
     private Renderer _renderer;
     private AudioManager audioManager;
 
-    void Start()
+    private ulong _tempPlayerId;
+
+    public void Init(float tempAngle, ulong playerId)
+    {
+        angle = tempAngle;
+        launcherId2 = new NetworkVariable<ulong>(playerId);
+    }
+
+    public override void OnNetworkSpawn()
     {
         //Shoot
         Vector2 force = new Vector2(shootSpeed * Mathf.Cos(angle), shootSpeed * Mathf.Sin(angle));
@@ -35,7 +43,7 @@ public class boule : NetworkBehaviour
         //Audio
         audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!IsServer) return;
